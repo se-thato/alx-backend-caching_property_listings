dcda097570d3862376e1de226e2cb6bb57006a47
se-thato <thatoselepe80@gmail.com>
2025-07-11 04:08:42 +0200

    cache property

--- alx_backend_caching_property_listings/properties/views.py
+++ alx_backend_caching_property_listings/properties/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Property
+from django.views.decorators.cache import cache_page
 
-# Create your views here.
+@cache_page(60 * 15)  # This will cache the response for 15 minutes
+def property_list(request):
+    properties = Property.objects.all().values()
+    return JsonResponse(list(properties), safe=False)
