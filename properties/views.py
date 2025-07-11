a5440461be5ebee025fd527349ccd8ae380057d9
se-thato <thatoselepe80@gmail.com>
2025-07-11 05:50:16 +0200

    made changes

--- properties/views.py
+++ properties/views.py
@@ -1,8 +1,11 @@
 from django.http import JsonResponse
-from .utils import get_all_properties
+from django.views.decorators.cache import cache_page
+from .models import Property
+from .utils import get_all_properties 
 
+@cache_page(60 * 15)  # Cache for 15 minutes
 def property_list(request):
-    properties = get_all_properties()
+    properties = Property.objects.all()
     data = [
         {
             "id": p.id,
