4e19420cf617609ec9d883c05a3b151724863604
se-thato <thatoselepe80@gmail.com>
2025-07-11 04:26:40 +0200

    signals

---
+++ alx_backend_caching_property_listings/properties/utils.py
@@ -0,0 +1,9 @@
+from django.core.cache import cache
+from .models import Property
+
+def get_all_properties():
+    properties = cache.get('all_properties')
+    if properties is None:
+        properties = list(Property.objects.all())
+        cache.set('all_properties', properties, 3600)  # Cache for 1 hour
+    return properties
--- alx_backend_caching_property_listings/properties/views.py
+++ alx_backend_caching_property_listings/properties/views.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render
 from django.http import JsonResponse
-from .models import Property
-from django.views.decorators.cache import cache_page
+from .utils import get_all_properties
 
-@cache_page(60 * 15)  # This will cache the response for 15 minutes
 def property_list(request):
-    properties = Property.objects.all().values()
-    return JsonResponse(list(properties), safe=False)
+    properties = get_all_properties()
+    data = [
+        {
+            "id": p.id,
+            "title": p.title,
+            "description": p.description,
+            "price": str(p.price),
+            "location": p.location,
+            "created_at": p.created_at.isoformat(),
+        }
+        for p in properties
+    ]
+    return JsonResponse(data, safe=False)
